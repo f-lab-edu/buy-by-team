1c1d07689549d2c5ef994714c99915c9446f2960
luvimperfection <luvimperfection@gmail.com>
2022-10-24 23:00:18 +0900

    Initialize spring project

---
+++ src/main/java/com/flab/bbt/BbtApplication.java
@@ -0,0 +1,13 @@
+package com.flab.bbt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbtApplication.class, args);
+	}
+
+}
