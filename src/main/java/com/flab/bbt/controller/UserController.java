9d5c0f805c063406fefb7d6766fe6f0324ce7c05
lion <lion@f-lab.kr>
2022-11-07 23:11:43 +0900

    controller

---
+++ src/main/java/com/flab/bbt/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.flab.bbt.controller;
+
+import com.flab.bbt.user.domain.User;
+
+public class UserController {
+    public userResponse getById(Long id){
+
+        User user = userSerivce.find...(id);
+        user -> userResponse
+        return userResponse
+        
+    }
+}
