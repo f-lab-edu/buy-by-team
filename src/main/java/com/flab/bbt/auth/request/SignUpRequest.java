21f2f68e2c78d7075f83c932dba9b7d2214db5b8
Hoon Park <55367357+hoonpark7442@users.noreply.github.com>
2022-11-14 20:36:16 +0900

    add annotation for validation to signUpRequest (#24)

--- src/main/java/com/flab/bbt/auth/request/SignUpRequest.java
+++ src/main/java/com/flab/bbt/auth/request/SignUpRequest.java
@@ -2,10 +2,18 @@ package com.flab.bbt.auth.request;
 
 import com.flab.bbt.user.domain.User;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 public class SignUpRequest {
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
+    @Email(message = "이메일 형식에 맞지 않습니다.")
     private String email;
+    @NotBlank(message = "패스워드는 필수 입력 값입니다.")
     private String password;
+    @NotBlank(message = "이름은 필수 입력 값입니다.")
     private String name;
+    @NotBlank(message = "핸드폰 번호는 필수 입력 값입니다.")
     private String phoneNo;
 
     public User convertToEntity(SignUpRequest request) {
