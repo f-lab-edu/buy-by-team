196384e3c71c3fffbf27f2420b4ffc77eb30f445
jinjinchoi <luvimperfection@gmail.com>
2022-11-14 21:45:37 +0900

    Add ProductEntity

---
+++ src/main/java/com/flab/bbt/product/domain/Product.java
@@ -0,0 +1,115 @@
+package com.flab.bbt.product.domain;
+
+import com.flab.bbt.user.domain.User;
+
+public class Product {
+    private long id;
+    private String name;
+    private String imgUrl;
+    private int priceSale;
+    private int priceDiscount;
+    private int discountRate;
+
+    public Product(String name, String imgUrl, int priceSale, int priceDiscount, int discountRate) {
+        this.name = name;
+        this.imgUrl = imgUrl;
+        this.priceSale = priceSale;
+        this.priceDiscount = priceDiscount;
+        this.discountRate = discountRate;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public int getPriceSale() {
+        return priceSale;
+    }
+
+    public void setPriceSale(int priceSale) {
+        this.priceSale = priceSale;
+    }
+
+    public int getPriceDiscount() {
+        return priceDiscount;
+    }
+
+    public void setPriceDiscount(int priceDiscount) {
+        this.priceDiscount = priceDiscount;
+    }
+
+    public int getDiscountRate() {
+        return discountRate;
+    }
+
+    public void setDiscountRate(int discountRate) {
+        this.discountRate = discountRate;
+    }
+
+    public static Product.ProductBuilder builder() {
+        return new Product.ProductBuilder();
+    }
+
+    public static class ProductBuilder {
+        private String name;
+        private String imgUrl;
+        private int priceSale;
+        private int priceDiscount;
+        private int discountRate;
+
+        ProductBuilder(){}
+
+        public Product.ProductBuilder name(String name) {
+            this.name = name;
+
+            return this;
+        }
+
+        public Product.ProductBuilder imgUrl(String imgUrl) {
+            this.imgUrl = imgUrl;
+
+            return this;
+        }
+
+        public Product.ProductBuilder priceSale(int priceSale) {
+            this.priceSale = priceSale;
+
+            return this;
+        }
+
+        public Product.ProductBuilder priceDiscount(int priceDiscount) {
+            this.priceDiscount = priceDiscount;
+            return this;
+        }
+
+        public Product.ProductBuilder discountRate(int discountRate) {
+            this.discountRate = discountRate;
+            return this;
+        }
+
+        public Product build() {
+            return new Product(this.name,this.imgUrl,this.priceSale,this.priceDiscount, this.discountRate);
+        }
+
+    }
+}
