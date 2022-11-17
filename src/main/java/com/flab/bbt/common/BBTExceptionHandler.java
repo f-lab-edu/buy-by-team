fa03c0ec6cad0be991d5d0c83e16682fc2256fda
hoon park <hoonpark7442@gmail.com>
2022-11-17 18:04:36 +0900

    add errors list field to CommonResponse to show each feild error

--- src/main/java/com/flab/bbt/common/BBTExceptionHandler.java
+++ src/main/java/com/flab/bbt/common/BBTExceptionHandler.java
@@ -4,10 +4,14 @@ import com.flab.bbt.exception.CustomException;
 import com.flab.bbt.exception.ErrorCode;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class BBTExceptionHandler {
@@ -19,6 +23,17 @@ public class BBTExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public CommonResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        return CommonResponse.fail(ErrorCode.INVALID_INPUT);
+        List<CommonResponse.FieldError> errors = buildFieldError(e.getBindingResult());
+
+        return CommonResponse.fail(ErrorCode.INVALID_INPUT, errors);
+    }
+
+    private List<CommonResponse.FieldError> buildFieldError(BindingResult result) {
+        return result.getFieldErrors().stream()
+                               .map(error -> CommonResponse.FieldError.builder()
+                                       .field(error.getField())
+                                       .message(error.getDefaultMessage())
+                                       .build())
+                               .collect(Collectors.toList());
     }
 }
