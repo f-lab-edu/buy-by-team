c0f2f30859139589ae1d307797fda94d9aeaa584
jinjinchoi <luvimperfection@gmail.com>
2022-11-21 08:02:37 +0900

    Add home controller with user authorization

--- src/main/java/com/flab/bbt/exception/ErrorCode.java
+++ src/main/java/com/flab/bbt/exception/ErrorCode.java
@@ -2,6 +2,7 @@ package com.flab.bbt.exception;
 
 public enum ErrorCode {
     USER_NOT_FOUND(3000, "유저를 찾지 못했습니다."),
+    USER_NOT_AUTHORIZED(3001, "권한이 없는 유저입니다."),
     INVALID_INPUT(4000, "입력값이 올바르지 않습니다.");
 
     long code;
---
+++ src/main/java/com/flab/bbt/home/controller/HomeController.java
@@ -0,0 +1,27 @@
+package com.flab.bbt.home.controller;
+
+import com.flab.bbt.common.CommonResponse;
+import com.flab.bbt.common.SessionManager;
+import com.flab.bbt.exception.CustomException;
+import com.flab.bbt.exception.ErrorCode;
+import com.flab.bbt.user.domain.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public CommonResponse authorizeUserBySession(
+            @SessionAttribute(name= SessionManager.COOKIE_SESSION_ID, required = false) User user){
+        if(user==null){
+            // authorized user does not exist! redirect to login view
+            throw new CustomException(ErrorCode.USER_NOT_AUTHORIZED);
+        }else{
+            // authorized user exists!
+            return CommonResponse.success();
+        }
+    }
+}
