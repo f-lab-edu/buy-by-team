f588e9aacb373faf48323de9160007adbf7ab429
jinjinchoi <luvimperfection@gmail.com>
2022-11-21 08:02:37 +0900

    Resolve merge conflict, fix error codes

--- src/main/java/com/flab/bbt/exception/ErrorCode.java
+++ src/main/java/com/flab/bbt/exception/ErrorCode.java
@@ -1,11 +1,16 @@
 package com.flab.bbt.exception;
 
 public enum ErrorCode {
+    // User/Auth
     USER_NOT_FOUND(3000, "유저를 찾지 못했습니다."),
     INVALID_INPUT(3001, "입력값이 올바르지 않습니다."),
     DUPLICATE_EMAIL(3002, "이메일이 중복되었습니다."),
+    USER_NOT_AUTHORIZED(3003, "권한이 없는 유저입니다."),
+
+    // Product
     PRODUCT_NOT_FOUND(5000, "상품을 찾지 못했습니다."),
     PRODUCT_ALREADY_EXISTS(5001, "이미 존재하는 상품입니다.");
+
     long code;
     String message;
 
