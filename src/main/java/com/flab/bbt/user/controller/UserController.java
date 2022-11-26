c330e73ea09c29e99b6c386affda922b8deb6312
hoon park <hoonpark7442@gmail.com>
2022-11-26 23:16:24 +0900

    update시 session 이용하여 유저 정보 조회

--- src/main/java/com/flab/bbt/user/controller/UserController.java
+++ src/main/java/com/flab/bbt/user/controller/UserController.java
@@ -1,31 +1,32 @@
 package com.flab.bbt.user.controller;
 
 import com.flab.bbt.common.CommonResponse;
+import com.flab.bbt.common.SessionConst;
 import com.flab.bbt.user.domain.User;
 import com.flab.bbt.user.domain.UserProfile;
 import com.flab.bbt.user.request.UpdateUserRequest;
 import com.flab.bbt.user.service.UserService;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
-    @PatchMapping("/{id}/user-profile")
-    public CommonResponse updateUserProfiile(@RequestBody UpdateUserRequest request,
-        @PathVariable long id
-        ) {
-        User user = userService.findUserById(id);
-        UserProfile userProfile = request.convertToUserProfile();
+    @PatchMapping("/user-profiles")
+    public CommonResponse updateUserProfiile(@RequestBody UpdateUserRequest updateUserRequest,
+        HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        User user = (User) session.getAttribute(SessionConst.COOKIE_SESSION_ID);
 
+        UserProfile userProfile = updateUserRequest.convertToUserProfile();
         userService.update(user, userProfile);
 
         return CommonResponse.success();
