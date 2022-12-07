5ea3ce0fba4ff92908f5983be5008c0eca1df109
jinjinchoi <luvimperfection@gmail.com>
2022-12-07 21:42:56 +0900

    Separate MyBatisUserRepositoryImpl

--- src/main/java/com/flab/bbt/user/repository/UserRepositoryImpl.java
+++ src/main/java/com/flab/bbt/user/repository/UserRepositoryImpl.java
@@ -1,27 +1,18 @@
 package com.flab.bbt.user.repository;
 
-import com.flab.bbt.exception.CustomException;
-import com.flab.bbt.exception.ErrorCode;
 import com.flab.bbt.user.domain.User;
-import com.flab.bbt.user.domain.UserProfile;
-import com.flab.bbt.user.repository.mybatis.UserMapper;
-import java.util.List;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Repository;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@Repository
 @RequiredArgsConstructor
 public class UserRepositoryImpl implements UserRepository {
 
-    private final UserMapper userMapper;
     private static Map<Long, User> userDb = new ConcurrentHashMap<>();
 
     // 이메일조회 성능을 위한 인덱스 대용 해시맵
@@ -30,7 +21,6 @@ public class UserRepositoryImpl implements UserRepository {
 
     @Override
     public void save(User user) {
-//        userMapper.save(user);
         user.setId(sequence.incrementAndGet());
         userDb.put(user.getId(), user);
         userEmailIndex.put(user.getEmail(), user.getId());
@@ -38,7 +28,6 @@ public class UserRepositoryImpl implements UserRepository {
 
     @Override
     public Optional<User> findById(Long id) {
-//        return userMapper.findById(id);
         return Optional.ofNullable(userDb.get(id));
     }
 
@@ -51,8 +40,6 @@ public class UserRepositoryImpl implements UserRepository {
         } else {
             return Optional.empty();
         }
-//        List<User> result = userMapper.findByEmail(email);
-//        return resultisEmpty() ? Optional.empty() : Optional.of(result.get(0));
     }
 
     @Override
@@ -64,17 +51,11 @@ public class UserRepositoryImpl implements UserRepository {
         } else {
             return Optional.empty();
         }
-
-//        List<User> result = userMapper.findByEmailAndPassword(email, password);
-//        log.info("result", result);
-//        return result.isEmpty() ? Optional.empty() : Optional.of(result.get(0));
     }
 
     @Override
     public User update(User user) {
-//        userDb.replace(user.getId(), user);
-
-        userMapper.update(user.getId(), user);
+        userDb.replace(user.getId(), user);
         return user;
     }
 }
