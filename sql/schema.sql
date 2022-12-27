b098ce14a4e6126c08342131bc53a1d2c8c77f02
hoonpark7442 <hoonpark7442@gmail.com>
2022-12-27 18:52:43 +0900

    schema.sql에 deal, price_table 추가

--- sql/schema.sql
+++ sql/schema.sql
@@ -26,3 +26,29 @@ create table if not exists mysqldb.product (
                          PRIMARY KEY(id),
                          index index_product_on_sku_code (sku_code)
 );
+
+create table if not exists mysqldb.deal (
+                         id INT NOT NULL AUTO_INCREMENT,
+                         product_id int,
+                         group_size int,
+                         discount_price int,
+                         status varchar(255),
+                         participant_count int,
+                         is_private boolean,
+                         expired_at TIMESTAMP,
+                         closed_at TIMESTAMP,
+                         PRIMARY KEY(id),
+                         index index_deal_on_status (status),
+                         foreign key (product_id) references product(id)
+);
+
+create table if not exists mysqldb.price_table (
+                         id INT NOT NULL AUTO_INCREMENT,
+                         product_id int,
+                         group_size int,
+                         discount_price int,
+                         target_period int,
+                         PRIMARY KEY(id),
+                         foreign key (product_id) references product(id)
+);
+
