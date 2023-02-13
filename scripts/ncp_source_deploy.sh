ae4902e0c0a032308a8d04ed67e6f4222c437d79
hoonpark7442 <hoonpark7442@gmail.com>
2023-02-13 13:18:42 +0900

    ncp_source_deploy.sh 수정

--- scripts/ncp_source_deploy.sh
+++ scripts/ncp_source_deploy.sh
@@ -20,14 +20,10 @@ function makeSignature() {
 	SIG+="$ACCESSKEY"
 
 	SIGNATURE=$(echo -n -e "$SIG"|iconv -t utf8 |openssl dgst -sha256 -hmac $SECRETKEY -binary|openssl enc -base64)
-
-	echo "${SIGNATURE}"
 }
-
-result=$(makeSignature)
 
 curl -i -X POST https://vpcsourcedeploy.apigw.ntruss.com$URI \
           -H "x-ncp-apigw-timestamp: $TIMESTAMP" \
           -H "x-ncp-iam-access-key: $ACCESSKEY" \
-          -H "x-ncp-apigw-signature-v2: $result"
+          -H "x-ncp-apigw-signature-v2: $SIGNATURE"
 
