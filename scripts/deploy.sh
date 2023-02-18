7089fd768dbb84cf5fea70808b5940c29751693d
hoonpark7442 <hoonpark7442@gmail.com>
2023-02-18 18:05:08 +0900

    현재 운영중인 pid 꺼지지 않는 현상 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -22,12 +22,12 @@ IDLE_PORT=$(find_idle_port)
 echo "> $IDLE_PORT 에서 구동중인 애플리케이션 pid 확인"
 IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
 
-if [ -z IDLE_PID ]
+if [ -z $IDLE_PID ]
 then
   echo "> Nothing to end."
 else
   echo "> kill -9 IDLE_PID"
-  kill -15 IDLE_PID
+  kill -15 $IDLE_PID
   sleep 5
 fi
 
