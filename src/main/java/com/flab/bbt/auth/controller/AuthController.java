eeb02cfe2f4d87647db7ca91088f0dd400ab9565
jinjinchoi <luvimperfection@gmail.com>
2023-02-26 14:19:51 +0900

    add logutils

--- src/main/java/com/flab/bbt/auth/controller/AuthController.java
+++ src/main/java/com/flab/bbt/auth/controller/AuthController.java
@@ -5,16 +5,17 @@ import com.flab.bbt.auth.request.SignUpRequest;
 import com.flab.bbt.auth.service.AuthService;
 import com.flab.bbt.auth.service.PasswordEncrypter;
 import com.flab.bbt.common.CommonResponse;
+import com.flab.bbt.common.LoggerUtils;
 import com.flab.bbt.common.SessionConst;
 import com.flab.bbt.exception.ErrorCode;
 import com.flab.bbt.user.domain.User;
 import com.flab.bbt.user.response.UserSignUpResponse;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +27,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthController {
 
+    private static final Logger logger = LoggerUtils.getLogger(AuthController.class);
+
     private final AuthService authService;
     private final PasswordEncrypter passwordEncrypter;
 
@@ -34,25 +37,32 @@ public class AuthController {
     public CommonResponse signUp(@Valid @RequestBody SignUpRequest signUpRequest) {
         // 회원가입 진행
         User user = authService.signUp(signUpRequest.convertToEntityWith(
-                        passwordEncrypter.encrypt(signUpRequest.getPassword())));
+            passwordEncrypter.encrypt(signUpRequest.getPassword())));
 
         return CommonResponse.success(UserSignUpResponse.convertToUserSignUpResponse(user));
     }
 
+    //logger.info("debug message", user);
     @PostMapping("/signin")
     @ResponseStatus(HttpStatus.OK)
     public CommonResponse signIn(@Valid @RequestBody SignInRequest signInRequest,
         HttpServletRequest request) {
+        logger.info("Attempting to sign in user with email: {}", signInRequest.getEmail());
+
         // authenticate user
         User user = signInRequest.convertToEntityWith(
             passwordEncrypter.encrypt(signInRequest.getPassword()));
+
         Long userId = authService.authenticate(user);
 
         if (userId == null) {
+            logger.info("User not found with email: {}", signInRequest.getEmail());
             return CommonResponse.fail(ErrorCode.USER_NOT_FOUND);
         } else {
             HttpSession session = request.getSession();
             session.setAttribute(SessionConst.COOKIE_SESSION_ID, userId);
+            logger.info("User with email {} successfully signed in with user ID {}",
+                signInRequest.getEmail(), userId);
             return CommonResponse.success();
         }
     }
